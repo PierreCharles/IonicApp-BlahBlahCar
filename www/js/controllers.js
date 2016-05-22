4fd28a14f2c28f4c15b1cff4e8337b8c90dc4f96
picharles <pier.charles@hotmail.fr>
2016-05-22 22:53:47 +0200

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('searchCtrl', function($scope) {
+
+})
+   
+.controller('announcesCtrl', function($scope) {
+
+})
+   
+.controller('proposeARideCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('profilCtrl', function($scope) {
+
+})
+ 
